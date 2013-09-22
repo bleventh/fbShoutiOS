3f89113b6148049dfa4e865f2eccb38c6b5cf6f8
Scott Vanderlind <scott.vanderlind2@gmail.com>
2013-09-21 18:06:29 -0700

    Initial Commit

---
+++ LocalTalk/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LocalTalk
+//
+//  Created by Scott Vanderlind on 9/21/13.
+//  Copyright (c) 2013 Scott Vanderlind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LocalTalk/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LocalTalk
+//
+//  Created by Scott Vanderlind on 9/21/13.
+//  Copyright (c) 2013 Scott Vanderlind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
